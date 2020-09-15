d5bf49b70da25ab9aeb791f765a7a51c9e87bd77
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-09-14 20:36:33 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { WorldMap, Avatar, ResponsiveContext, Image, Clock, Grid, Box, Nav, Menu, Main, Heading, Paragraph, Footer, Text, Tabs, Tab, Button, Anchor, Header, Grommet, CardHeader, Card, CardFooter, CardBody } from 'grommet';
-import { Codepen, Github, Notification, Linkedin, ChatOption, Home, Favorite, ShareOption } from 'grommet-icons';
+import { WorldMap, Avatar, ResponsiveContext, Clock, Box, Nav, Main, Footer, Text, Anchor, Header, Grommet, CardHeader, Card, CardFooter, CardBody } from 'grommet';
+import { Codepen, Github, Linkedin } from 'grommet-icons';
 import './App.css';
 import Me from './Me.jpg'
 
@@ -8,7 +8,6 @@ const App = () => {
 
   return (
     <Grommet className='app'>
-
       <ResponsiveContext.Consumer>
         {size => (
           <Header className='header' background="brand">
@@ -24,17 +23,25 @@ const App = () => {
 
       <ResponsiveContext.Consumer>
         {size => (
-          <Main flex='grow' overflow='auto' fill='horizontal' pad="medium">
+          <Main flex='grow' overflow='auto' fill='horizontal' >
             <Box
               direction="row"
               pad="medium"
             >
               <Box pad="large" background="light-3" >
-                <Avatar
+                <Box direction='row'>
+                  <Avatar
+                    size='xlarge'
+                    fit="cover"
+                    src={Me}
+                    margin='small'
+                  /><h1 className='mainHeading'>Hi, I'm Dinesh.</h1>
+                </Box>
+                {/* <Avatar
                   size='xlarge'
                   fit="cover"
                   src={Me}
-                /><h1 className='mainHeading'>Hi, I'm Dinesh.</h1>
+                /><h1 className='mainHeading'>Hi, I'm Dinesh.</h1> */}
               If you're wondering if I'm the character from the HBO's comedy series 'Silicon Valley', I'm not. I'm a real person with the name Dinesh Tathekalva. I'm a Software Engineer, Frontend Engineer.
               I build web and mobile applications for financial and tech companies. I also love and study astronomy during my free time
               </Box>
@@ -43,34 +50,75 @@ const App = () => {
         )}
       </ResponsiveContext.Consumer>
 
-      <Box
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Main flex='grow' overflow='auto' fill='horizontal' >
+            <Box
         direction="row"
-        pad="large"
-        width='large'
-        responsive='true'
-        margin='small'
+        pad="medium"
       >
-        <WorldMap
-          color="neutral-1"
-          continents={[
-            {
-              name: 'North America',
-              color: 'dark-1',
-              onClick: (name) => { },
-            },
-          ]}
-          onSelectPlace={(lat, lon) => { }}
-          places={[
-            {
-              name: 'Sydney',
-              location: [37.7749, -122.4194],
-              color: 'accent-4',
-              onClick: (name) => { },
-            },
-          ]}
-          selectColor="accent-2"
-        />
+        <Card margin='small' align='center' height="medium" width="medium" background="light-1">
+          <CardHeader fill='horizontal' pad="medium" >Location</CardHeader>
+          <CardBody pad="medium">
+            <WorldMap
+              extend={(props) => {}}
+              color="dark-6"
+              continents={[
+                {
+                  name: 'North America',
+                  color: 'neutral-2',
+                  onClick: (name) => { },
+                },
+              ]}
+              onSelectPlace={(lat, lon) => { }}
+              places={[
+                {
+                  name: 'Bay Area',
+                  location: [38.8375, -122.2913],
+                  color: 'accent-1',
+                  onClick: (name) => { },
+                },
+              ]}
+              selectColor="accent-2"
+            />
+          </CardBody>
+          <CardFooter fill='horizontal' justify='center' pad='medium' background="light-3">
+            Bay Area, CA
+        </CardFooter>
+        </Card>
+
+        {/* <Card margin='small' align='center' height="medium" width="medium" background="light-1">
+          <CardHeader fill='horizontal' pad="medium" >Location</CardHeader>
+          <CardBody pad="medium">
+            <WorldMap
+              color="dark-6"
+              continents={[
+                {
+                  name: 'North America',
+                  color: 'neutral-2',
+                  onClick: (name) => { },
+                },
+              ]}
+              onSelectPlace={(lat, lon) => { }}
+              places={[
+                {
+                  name: 'Bay Area',
+                  location: [37.7749, -122.4194],
+                  color: '#FF4040',
+                  onClick: (name) => { },
+                },
+              ]}
+              selectColor="accent-2"
+            />
+          </CardBody>
+          <CardFooter fill='horizontal' justify='center' pad='medium' background="light-3">
+            Bay Area, CA
+        </CardFooter>
+        </Card> */}
       </Box>
+          </Main>
+        )}
+      </ResponsiveContext.Consumer>
 
       <ResponsiveContext.Consumer>
         {size => (
